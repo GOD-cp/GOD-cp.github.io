01379da31a6ac842698a0fe01ebd6402d959b5ab
GOD-cp <gp21a008@oecu.jp>
2024-09-06 00:59:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("まだ続き書いてないですぅ");
         });
     });
+
+
+const images = document.querySelectorAll("img");
+
+images.forEach(image => {
+    image.addEventListener("click", function() {
+        // displayが "none" かどうかをチェックして切り替え
+        if (image.style.display === "none") {
+            image.style.display = "block"; // 画像を表示する
+        } else {
+            image.style.display = "none";  // 画像を非表示にする
+        }
+    });
+});
 });
